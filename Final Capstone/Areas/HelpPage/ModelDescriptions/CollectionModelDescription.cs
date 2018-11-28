cc07091f1fb8c98c4ddac102fa78a4d778f5f69d
Jon DeRuiter <jonathanhderuiter@gmail.com>
2018-11-28 11:18:39 -0500

    database connection, api scaffolding, and basic info entered, initial commit

---
+++ Final Capstone/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Final_Capstone.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Final Capstone/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Final_Capstone.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
